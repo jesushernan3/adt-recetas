a094e645ed6df83a457738cdc59ee3792942d99d
Jesus Hernan <jesushernan3@gmail.com>
2020-03-25 20:55:36 -0300

    implementacion de estetica y limpieza de codigo para las vistas de categoria y subcategoria

--- vistas/recetas-crudas.php
+++ vistas/recetas-crudas.php
@@ -1,20 +1,107 @@
-
 <?php 
 $raiz="../";
 include('../estructura/head.php');?>
 
-<section class="container">
+<div class="categoria container">
+  <section class="section">
+    
+  <h1 class="title is-1">RECETAS CRUDAS</h1>
+    
+    <div class="columns">
+
+        <div class="column is-one-fifth">
+          <div class="card">
+            <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-ensaladas.php">
+              <div class="card-image">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+              </div>
+              <div class="card-content has-text-centered">
+                <h3>Ensaladas</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="column is-one-fifth">
+          <div class="card">
+          <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-aperitivos.php">
+              <div class="card-image">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+              </div>
+              <div class="card-content has-text-centered">
+                <h3>Aperitivos</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="column is-one-fifth">
+          <div class="card">
+          <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-aderezos.php">
+              <div class="card-image">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+              </div>
+              <div class="card-content has-text-centered">
+                <h3>Aderezos</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+        
+        <div class="column is-one-fifth">
+          <div class="card">
+          <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-sopas.php">
+              <div class="card-image">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+              </div>
+              <div class="card-content has-text-centered">
+                <h3>Sopas</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="column is-one-fifth">
+          <div class="card">
+          <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-bebidas.php">
+              <div class="card-image">
+                <figure class="image is-128x128">
+                  <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+              </div>
+              <div class="card-content has-text-centered">
+                <h3>Bebidas</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="column is-one-fifth">
+          <div class="card">
+            <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-principales.php">
+                <div class="card-image">
+                  <figure class="image is-128x128">
+                    <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                  </figure>
+                </div>
+                <div class="card-content has-text-centered">
+                  <h3>Principales</h3>
+                </div>
+            </a>
+          </div>
+        </div>
+
+    </div>
   
-<h1 class="title is-1">RECETAS CRUDAS</h1>
-  
-  <div class="contenedor-links-botones-medianos">
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas-ensaladas.php">Ensaladas</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas.php">Desayunos</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas.php">Aperitivos</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas.php">Aderezos</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-crudas.php">Sopas</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-cocinadas.php">Bebidas</a>
-  </div>
-</section>
+  </section>
+</div>
 
 <?php include('../estructura/foot.php');?>
--- vistas/recetas/recetas-crudas-ensaladas.php
+++ vistas/recetas/recetas-crudas-ensaladas.php
@@ -2,18 +2,24 @@
 <?php 
 $raiz="../../";
 include('../../estructura/head.php');?>
-
-<div class="alineacion-centrado">
-  <h1 class="titulo-seccion">RECETAS/CRUDAS</h1>
+<div class="subcategoria container">
+  <section class="section articulos">
+    
+    <h1 class="titulo-seccion">ENSALADAS</h1>
+    
+    <article class="card">
+      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-02-ceviche.php">
+        <div class="card-image"><figure><img src="http://lorempixel.com/food/300/200" alt="#"></figure></div>
+        <div class="card-header"><h3>Ceviche de Champiñones</h3></div>
+        <div class="card-content">
+          <div class="content"> Este plato no puede faltar entre tus opciones de cocina vegetariana, es rico en sabor, nutrientes y completamente diferente. Este sin duda es uno de los más ricos ceviches de vegetales, hace un tiempo publicamos una opción de ceviche con soya entre otros, los cuales se … </div>
+        </div>
+        <div class="spacer"></div>
+      </a>
+    </article>
+          <!-- <a href="//<?php echo $raiz?>vistas/recetas/recetas-id-01.php">De Berros</a> -->
+    
+  </section>
 </div>
-  
-  <div class="contenedor-links-botones-medianos">
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-01.php">De Berros</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-02.php">De Frijoles Blancos</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-03.php">De Remolacha</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-04.php">De Zanahoria</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-05.php">Cebolla y Pera</a>
-      <a href="<?php echo $raiz?>vistas/recetas/recetas-id-06.php">Ajo </a>
-  </div>
 
 <?php include('../../estructura/foot.php');?>
