d7f16bc14d480025bc2ec22b212ef783445657bf
Jesus Hernan <jesushernan3@gmail.com>
2020-04-15 16:54:02 -0300

    idem anterior

--- app/views/home.php
+++ app/views/home.php
@@ -2,10 +2,23 @@
   $raiz = '/';
   include 'includes/head.php';
 ?>
+<section id="main-banner" class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+      This is Home View
+      </h1>
+      <h2 class="subtitle">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fuga officiis quisquam voluptas porro, omnis libero fugit mollitia quos esse. Ut in consequuntur earum esse maiores magnam. Atque, placeat qui
+      </h2>
+    </div>
+  </div>
+</section>
 <main>
   <h1>
-  home
+  
   </h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam fuga officiis quisquam voluptas porro, omnis libero fugit mollitia quos esse. Ut in consequuntur earum esse maiores magnam. Atque, placeat qui.</p>
 </main>
 
 <?php include 'includes/foot.php'; ?>
