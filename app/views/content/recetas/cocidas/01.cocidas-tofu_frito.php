d93a331de03c49c81643e40462a59ee95cdfadcd
jesus hernan <jesushernan@gmail.com>
2020-04-17 08:25:39 -0300

    correcion de imagenes en los banners de articulos y miniaturas

--- app/views/content/recetas/cocidas/01.cocidas-tofu_frito.php
+++ app/views/content/recetas/cocidas/01.cocidas-tofu_frito.php
@@ -4,54 +4,63 @@
 ?>
 
 <main class="container content">
-  
-  <section class="hero banner articulo-banner-tofu-frito is-medium">
+
+  <section class="hero banner is-medium">
     <div class="hero-body">
-        <div class="container">
-          <h2 class="subtitle">
+      <figure class="image is-fullwidth">
+        <img src="<?php echo $raiz;?>public/img/recetas/cocidas/tofu-frito.jpg">
+      </figure>
+      <div class="container">
+        <h2 class="subtitle">
           recetas cocidas
-          </h2>
-          <h1 class="title">
+        </h2>
+        <h1 class="title">
           Tofu Frito
-          </h1>
-        </div>
+        </h1>
+      </div>
     </div>
   </section>
 
- <section class="section">
-   <p>Una receta simple, rápida e igualmente exquisita para preparar con poco tiempo es freír el tofu y acompañarlo con arroces y pastas, ensaladas, u otros, pero, ¿como freír el tofu y dejarlo bien sabroso? -ya que el tofu frito sólo queda realmente muy desabrido- pues aquí una magnifica y muy simple idea. Aprende paso a paso a conseguir el mejor tofu frito dejándolo con un sabor delicioso sin problemas, con instrucciones paso a paso.</p>
- </section>
+  <section class="section">
+    <p>Una receta simple, rápida e igualmente exquisita para preparar con poco tiempo es freír el tofu y acompañarlo con
+      arroces y pastas, ensaladas, u otros, pero, ¿como freír el tofu y dejarlo bien sabroso? -ya que el tofu frito sólo
+      queda realmente muy desabrido- pues aquí una magnifica y muy simple idea. Aprende paso a paso a conseguir el mejor
+      tofu frito dejándolo con un sabor delicioso sin problemas, con instrucciones paso a paso.</p>
+  </section>
 
- <section class="section content">
-  <h3>
-    Ingredientes
-  </h3>
+  <section class="section content">
+    <h3>
+      Ingredientes
+    </h3>
     <ul>
-      
-    <li>Tofu (Extra firme, consistencia dura)</li>
-    <li>Salsa de Soya</li>
-    <li>Fécula de Maíz</li>
-    <li>Cebollino (Cebollín)</li>
-    <li>Aceite Vegetal</li>
+
+      <li>Tofu (Extra firme, consistencia dura)</li>
+      <li>Salsa de Soya</li>
+      <li>Fécula de Maíz</li>
+      <li>Cebollino (Cebollín)</li>
+      <li>Aceite Vegetal</li>
 
     </ul>
- </section>
+  </section>
 
- <section class="section content">
-  <h3>
-    Preparación
-  </h3>
+  <section class="section content">
+    <h3>
+      Preparación
+    </h3>
     <ul>
-      <li>La preparación de esta receta de cocina vegetariana es muy fácil. Primero quitaremos el máximo de líquido posible del tofu, secando con toallas papel absorbente.</li>
+      <li>La preparación de esta receta de cocina vegetariana es muy fácil. Primero quitaremos el máximo de líquido
+        posible del tofu, secando con toallas papel absorbente.</li>
       <li>Luego cortaremos el tofu en cuadros o el tiras largas como de patatas fritas.</li>
-      <li>Ahora pasaremos el tofu picado por la fécula de maíz, por todas las caras, de manera uniforme y finalmente pasaremos a freír en aceite hirviendo.</li>
+      <li>Ahora pasaremos el tofu picado por la fécula de maíz, por todas las caras, de manera uniforme y finalmente
+        pasaremos a freír en aceite hirviendo.</li>
       <li>Freír con llama baja, mientras agregamos tiras picadas de cebollino fresco.</li>
-      <li>Una vez bien dorado el tofu frito, retirar y dejar en papel absorbente para eliminar los excesos de aceite.</li>
+      <li>Una vez bien dorado el tofu frito, retirar y dejar en papel absorbente para eliminar los excesos de aceite.
+      </li>
       <li>Servir el tofu con acompañamientos a gusto y salsa de soya para condimentar en el plato.</li>
       <li>Puedes conservar el tofu frito por dos días, manteniendo refrigerado.</li>
     </ul>
- </section>
- 
+  </section>
+
 </main>
 
 <?php include '../../../includes/foot.php'; ?>
